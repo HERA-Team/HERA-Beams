0a06aedf94a2b7e6aa2eb8d7632429c5e9d34512
Bryna Hazelton <bryna.hazelton@gmail.com>
2018-04-03 17:07:47 -0700

    Added several options to script, updated readme

--- NicolasFagnoniBeams/convert_to_uvbeam.py
+++ NicolasFagnoniBeams/convert_to_uvbeam.py
@@ -2,11 +2,22 @@ import os
 from glob import glob
 import subprocess
 import argparse
+import numpy as np
 from pyuvdata import UVBeam
 
 a = argparse.ArgumentParser(description="A command-line script to convert Nicolas "
                             "Fagnoni's CST simulations to UVBeam FITS files.")
-a.add_argument("nf_repo_path", type=str, help="Path to Nicolas Fagnoni's git repo")
+a.add_argument('nf_repo_path', type=str, help="Path to Nicolas Fagnoni's git repo")
+a.add_argument('--efield', help='Efield rather than power beams',
+               action='store_true', default=False)
+a.add_argument('--calc_cross_pols', help='Calculate cross pol power beams '
+               '(i.e. xy, yx). Only applies if efield is not True.',
+               action='store_true', default=False)
+a.add_argument('--healpix', help='Convert to HEALPix',
+               action='store_true', default=True)
+a.add_argument('--outfile', type=str, help='Output file name', default=None)
+a.add_argument('-f', '--freq_range', nargs=2, type=float,
+               help='Frequency range to include in MHz')
 
 args = a.parse_args()
 nf_repo_path = os.path.expanduser(args.nf_repo_path)
@@ -31,15 +42,49 @@ version_str = ('  Git origin: ' + git_origin +
                '.  Git hash: ' + git_hash + '.')
 
 beam = UVBeam()
-beam.read_cst_beam(beam_files, beam_type='efields', telescope_name='HERA',
+default_out_file = 'NF_HERA'
+
+if args.freq_range is not None:
+    from pyuvdata.cst_beam import CSTBeam
+    beam1 = CSTBeam()
+    frequencies = [beam1.name2freq(f) for f in beam_files]
+    # convert to MHz
+    frequencies = np.array(frequencies) / 1e6
+    files_to_use = np.where((frequencies >= args.freq_range[0]) &
+                            (frequencies <= args.freq_range[1]))[0]
+    if files_to_use.size == 0:
+        raise ValueError('No files included in freq_range.')
+
+    beam_files = [beam_files[f] for f in files_to_use]
+
+if args.efield or args.calc_cross_pols:
+    read_beam_type = 'efield'
+else:
+    read_beam_type = 'power'
+
+if args.efield:
+    default_out_file += '_efield'
+else:
+    default_out_file += '_power'
+default_out_file += '_beam'
+
+beam.read_cst_beam(beam_files, beam_type=read_beam_type, telescope_name='HERA',
                    feed_name='PAPER_dipole', feed_version='0.1',
                    model_name=model_name, model_version='1.0')
 
+if not args.efield and args.calc_cross_pols:
+    beam.efield_to_power()
+
 beam.history = 'CST simulations by Nicolas Fagnoni.' + version_str
-beam.write_beamfits("NF_HERA_efield_beam.fits", clobber=True)
 
-beam.efield_to_power()
-beam.write_beamfits("NF_HERA_power_beam.fits", clobber=True)
+if args.healpix:
+    beam.az_za_to_healpix()
+    default_out_file += '_healpix'
 
-beam.az_za_to_healpix()
-beam.write_beamfits("NF_HERA_power_beam_healpix.fits", clobber=True)
+default_out_file += '.fits'
+if args.outfile is not None:
+    outfile = args.outfile
+else:
+    outfile = default_out_file
+
+beam.write_beamfits(outfile, clobber=True)
